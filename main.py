4efdba7661eb45017969f48639e032a971f76101
celfcelfcelfcelf <98778688+celfcelfcelfcelf@users.noreply.github.com>
2023-05-24 22:24:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1283,8 +1283,8 @@ if st.session_state.game_started == False:
 
 
 
-if col3.button('Start new game'):
-    st.session_state.game_started == False
+#if col3.button('Start new game'):
+#    st.session_state.game_started == False
     #st.write('Uphill:', flatlist)
 #human_chooses_cards(st.session_state.cards, st.session_state.riders)
 
